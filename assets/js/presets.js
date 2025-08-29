0895b8d266123686499ba6cd91ac81ad617e2ce7
ronkuby-mage <54195730+ronkuby-mage@users.noreply.github.com>
2025-08-29 19:10:43 -0400

    Fix overview resizing
    plus a little preset cleanup/additions

--- assets/js/presets.js
+++ assets/js/presets.js
@@ -332,7 +332,28 @@ export default {
             ranged: { item_id: 21603, enchant_id: null }
         }
     }, {
-        name: "Naxxramas",
+        name: "Phase 6 Enter (w/ UDC)",
+        loadout: {            
+            head: { item_id: 21347, enchant_id: 24164 },
+            neck: { item_id: 21608, enchant_id: null },
+            shoulder: { item_id: 19370, enchant_id: 24421 },
+            back: { item_id: 22731, enchant_id: null },
+            chest: { item_id: 23085, enchant_id: 20025 },
+            wrist: { item_id: 23091, enchant_id: 20008 },
+            hands: { item_id: 23084, enchant_id: 25078 },
+            waist: { item_id: 22730, enchant_id: null },
+            legs: { item_id: 21676, enchant_id: 24164 },
+            feet: { item_id: 21344, enchant_id: 13890 },
+            finger1: { item_id: 21709, enchant_id: null },
+            finger2: { item_id: 19403, enchant_id: null },
+            trinket1: { item_id: 19379, enchant_id: null },
+            trinket2: { item_id: 19339, enchant_id: null },
+            main_hand: { item_id: 21622, enchant_id: 22749 },
+            off_hand: { item_id: 21597, enchant_id: null },
+            ranged: { item_id: 21603, enchant_id: null }
+        }
+    }, {
+        name: "Phase 6 Exit",
         loadout: {            
             head: { item_id: 22498, enchant_id: 24164 },
             neck: { item_id: 21608, enchant_id: null },
@@ -353,7 +374,7 @@ export default {
             ranged: { item_id: 22821, enchant_id: null }
         }
     }, {
-        name: "Naxxramas (Undead)",
+        name: "Phase 6 Exit (w/ UDC)",
         loadout: {
             head: { item_id: 22498, enchant_id: 24164 },
             neck: { item_id: 21608, enchant_id: null },
@@ -372,6 +393,27 @@ export default {
             main_hand: { item_id: 22589, enchant_id: 22749 },
             off_hand: { item_id: null, enchant_id: null },
             ranged: { item_id: 22820, enchant_id: null }
+        }
+    }, {
+        name: "Two Mages",
+        loadout: {            
+            head: { item_id: 22498, enchant_id: 24164 },
+            neck: { item_id: 23057, enchant_id: null },
+            shoulder: { item_id: 22983, enchant_id: 29467 },
+            back: { item_id: 23050, enchant_id: null },
+            chest: { item_id: 22496, enchant_id: 20025 },
+            wrist: { item_id: 21186, enchant_id: 20008 },
+            hands: { item_id: 21585, enchant_id: 25078 },
+            waist: { item_id: 22730, enchant_id: null },
+            legs: { item_id: 23070, enchant_id: 24164 },
+            feet: { item_id: 22500, enchant_id: 13890 },
+            finger1: { item_id: 23237, enchant_id: null },
+            finger2: { item_id: 23025, enchant_id: null },
+            trinket1: { item_id: 23207, enchant_id: null },
+            trinket2: { item_id: 23046, enchant_id: null },
+            main_hand: { item_id: 22589, enchant_id: 22749 },
+            off_hand: { item_id: null, enchant_id: null },
+            ranged: { item_id: 22821, enchant_id: null }
         }
     }],
 };
